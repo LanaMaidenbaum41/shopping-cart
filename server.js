faba403db19cf4b782a5082ebaa080d13a7e6b28
Dan Cohen <dan74c@gmail.com>
2017-09-13 16:58:32 +0300

    completed modal

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.listen(8000);
+app.listen(8001);
 
 app.use(express.static('public'));
 app.use(express.static(__dirname + '/node_modules'));
