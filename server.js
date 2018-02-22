b3fc64f5bdba9fdba16bc0dca7a1d40172eb68f3
Lana Maidenbaum <lmaidenbaum@gmail.com>
2018-02-22 16:07:29 -0500

    updated

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.listen(8001);
+app.listen(8001, function(){
+    console.log("listening on 8001");
+});
 
 app.use(express.static('public'));
 app.use(express.static(__dirname + '/node_modules'));
